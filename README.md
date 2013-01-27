892b9b08902ad343756573ca4fe447550848479f
Vera Yin <verayin@gmail.com>
2013-01-27 00:35:21 -0500

    initial commit: meadow interface and web.py backend

---
+++ README.md
@@ -0,0 +1,6 @@
+meadow
+==========
+
+meadow is an interface and server for a [quoridor](https://en.wikipedia.org/wiki/Quoridor)-like game featuring two AI agents.
+
+it uses [raphael.js](http://raphaeljs.com) to render SVG and [web.py](http://webpy.org) on the backend.
---
+++ code.py
@@ -0,0 +1,37 @@
+import web, daisy, lilac
+
+urls = ('/', 'index')
+
+class index:
+    def GET(self):
+        q = web.ctx.query
+        if not q:
+            return 'nada'
+        else:
+            p = q.split(':')
+            if len(p)==1 or not p[1]:
+                history = []
+            else:
+                history = p[1].split(',')
+            if p[0] == '?search':
+                try:
+                    pawn_moves = [str(x[0]*9+x[1]) for x in list(daisy.pawn_moves(history))]
+                    return ','.join(pawn_moves+list(daisy.wall_moves(history)))
+                except:
+                    print 'search oops'
+                    return ''
+            elif p[0] == '?daisy':
+                player = daisy
+            elif p[0] == '?lilac':
+                player = lilac
+            else:
+                return 'unknown player'
+            try:
+                return player.next_move(history)
+            except:
+                print 'move oops'
+                return ''
+
+if __name__ == '__main__':     
+    app = web.application(urls, globals())
+    app.run()
